4123d019afca8dae00d4796cba76c1fad8b3dede
Aniket Deole <aniket.deole@gmail.com>
2015-03-17 05:09:43 -0400

    Added Toolbar.

--- android/Procyon/app/src/main/java/com/procyon/procyon/Intro.java
+++ android/Procyon/app/src/main/java/com/procyon/procyon/Intro.java
@@ -2,6 +2,7 @@ package com.procyon.procyon;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,10 @@ public class Intro extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
     }
 
 
